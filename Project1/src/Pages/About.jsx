568ba6f60221215edffe9cfde9d9a982efd6e584
Jay Ambadkar <jambadkar@icloud.com>
2024-09-23 18:18:22 +0100

    make about page more detailed

--- Project1/src/Pages/About.jsx
+++ Project1/src/Pages/About.jsx
@@ -6,10 +6,23 @@ export default function About() {
   useEffect(() => {
     document.title = title
   }, [title])
-  
+
+  const exampleTasks = [
+    "Replying to important emails",
+    "Shopping for specific items",
+    "Doing the admin stuff you never feel like doing",
+    "And so much more!"
+  ]
+
   return (
       <>
         <h3>About this website</h3>
+        <p>This site is designed to help you out with remembering your everyday, non-routine, tasks which can include stuff like: </p>
+        <ul>
+          {exampleTasks.map((task, index) => {
+            return <li key={index}>{task}</li>
+          })}
+        </ul>
       </>
   )
 }
